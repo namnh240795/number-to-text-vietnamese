262f195dbc579d5a0c62dc4086af0cf3a7c9a1e8
Nguyen Hoai Nam <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-08-07 15:41:21 +0700

    update test case for negative number

--- __test__/single.test.js
+++ __test__/single.test.js
@@ -31,6 +31,37 @@ test('9999999999', () => {
   expect(getText(9999999999)).toBe('chín tỷ chín trăm chín mươi chín triệu chín trăm chín mươi chín nghìn chín trăm chín mươi chín');
 })
 
-test('-123', () => {
-  expect(getText(-123)).toBe('âm một trăm hai mươi ba');
-});
+for (let i = 1; i< 10; i++) {
+  test(`-${i}`, () => {
+    expect(getText(-i)).toBe(`âm ${base[i]}`);
+  });
+}
+
+test(`âm năm mươi tư`, () => {
+  expect(getText(-54)).toBe('âm năm mươi tư');
+})
+
+test('âm một trăm linh tư nghìn năm trăm', () => {
+  expect(getText(-104500)).toBe('âm một trăm linh tư nghìn năm trăm');
+})
+
+test('âm 5000909', () => {
+  expect(getText(-5000909)).toBe('âm năm triệu chín trăm linh chín');
+})
+
+test('âm 5000000909', () => {
+  expect(getText(-5000000909)).toBe('âm năm tỷ chín trăm linh chín');
+})
+
+test('âm 5000100909', () => {
+  expect(getText(-5000100909)).toBe('âm năm tỷ một trăm nghìn chín trăm linh chín');
+})
+
+test('âm 9999999999', () => {
+  expect(getText(-9999999999)).toBe('âm chín tỷ chín trăm chín mươi chín triệu chín trăm chín mươi chín nghìn chín trăm chín mươi chín');
+})
+
+test('- 0', () => {
+  expect(getText(-0)).toBe('không');
+})
+
